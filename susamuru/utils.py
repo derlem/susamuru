c13cc2524c11dbebe3ffdabf18aaa965efa3b46d
pxfurkanenes <furkanyaln9@gmail.com>
2019-03-18 10:15:37 +0300

    strip_ambiguous_term name fixed

--- susamuru/utils.py
+++ susamuru/utils.py
@@ -1,4 +1,4 @@
-def strip_disambiguation_term(disamb_title, disambiguation):
+def strip_ambiguous_term(disamb_title, disambiguation):
     title = disamb_title.strip(" " + disambiguation).lower()
     if title == disamb_title:
         title = disamb_title.strip(disambiguation).lower()
